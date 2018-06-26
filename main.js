1a9d82a50d696b3d6dfa3e0bf363903f67bbfc00
Ahale43 <haleau@gmail.com>
2018-06-25 23:43:45 -0400

    test commit

--- main.js
+++ main.js
@@ -104,8 +104,8 @@ function incrAArtists() {
             ; break;
         case 3:
             ; break;
-        case 4:
-            ; break;
+        // case 4:
+        //     ; break;
         case 5:
             ; break;
         case 6:
