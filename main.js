a721c5c622c93833cffcaa53df8f71d9c2a97b8c
Ahale43 <haleau@gmail.com>
2018-06-26 21:39:35 -0400

    functionality

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function refresh() {
     document.getElementById('answerB').innerHTML = getAnswerChoices();
     document.getElementById('answerC').innerHTML = getAnswerChoices();
     document.getElementById('answerD').innerHTML = getAnswerChoices();
-   
+
     // Uncheck the radio buttons for each question
     document.getElementById('ansA').checked = false;
     document.getElementById('ansB').checked = false;
@@ -19,10 +19,10 @@ function refresh() {
 }
 
 var answer_q_and_a = [
-    ["0-17", "Earbuds", "Fast", "Action", "Go out and party", "I'm with it", "something", "something", "East Coast", "Hooks make the song"],
-    ["18-24", "Over-the-ear headphones", "Medium paced", "Chill with some friends", "Keep it out of music", "somethingb", "somethingb", "West Coast", "Little to no hooks. I like it old school."],
-    ["25-39", "Small speakers", "Smooth and slow", "Comedy", "Stay home alone", "As long as it isn't overbearing, it's good", "somC", "somCC", "Southern Rap", "Both"],
-    ["40+", "Huge speakers", "Other/Unique", "Thriller", "Play video games", "Not a huge fan, but I'll listen sometimes", "sD1", "sD2", "Area doesn't matter", "Neither. I like production."]
+    ["0-17", "Earbuds", "Fast", "Action", "Go out and party", "I'm with it", "T-Pain is my idol", "J. Cole", "East Coast", "Hooks make the song"],
+    ["18-24", "Over-the-ear headphones", "Medium paced", "Horror", "Chill with some friends", "Keep it out of music", "I think it's great", "Drake", "West Coast", "Little to no hooks. I like it old school."],
+    ["25-39", "Small speakers", "Smooth and slow", "Comedy", "Stay home alone", "As long as it isn't overbearing, it's good", "Not a huge fan", "Kendrick Lamar", "Southern Rap", "Both"],
+    ["40+", "Huge speakers", "Other/Unique", "Thriller", "Play video games", "Not a huge fan, but I'll listen sometimes", "I hate autotune. I'm old", "I don't listen to mainstream artists", "Area doesn't matter", "Neither. I like production."]
 ];
 
 var question_number = 1;
@@ -32,7 +32,6 @@ var first_question = false;
 function getAnswerChoices() {
     if (first_question) { idx++ };
 
-    var answer_ind = "";
     while (idx < answers.length) {
         switch (answers[idx].value) {
             case 'a':
@@ -52,10 +51,17 @@ function getAnswerChoices() {
 }
 
 // List of all possible artist results
-var amir_obe, sir, jalen_santoy = 0;
-var the88glam, derek_pope = 0;
-var cunninlynguists, joyner_lucas, royce_da_59 = 0;
-var flatbush_zombies, kirk_knight, nyck_caution = 0;
+var amir_obe = 0
+var sir = 0
+var jalen_santoy = 0; 
+var the88glam = 0;
+var derek_pope = 0;
+var cunninlynguists = 0;
+var joyner_lucas = 0
+var royce_da_59 = 0 
+var flatbush_zombies = 0; 
+var kirk_knight = 0;
+var nyck_caution = 0;
 
 function submitAnswer() {
     var i = 0, options = answers.length;
@@ -90,34 +96,61 @@ function submitAnswer() {
         default:
             return;
     }
+
     question_number++;
     idx = 0;
     refresh();
-
+    if (question_number==10) {
+        document.getElementById('button').innerHTML="Submit Quiz";
+    }
+    if (question_number>10) {
+        getResults();
+    }
 }
+
+function getResults() {
+    var artist_picks = {"Amir Obe": amir_obe, "SiR": sir, "Jalen Santoy": jalen_santoy, 
+    "Derek Pope": derek_pope, "88GLAM": the88glam, "Cunninlynguists": cunninlynguists, 
+    "Joyner Lucas": joyner_lucas, "Royce da 5'9\"": royce_da_59, "Flatbush Zombies": flatbush_zombies, 
+    "Kirk Knight": kirk_knight, "Nyck Caution": nyck_caution};
+
+    var artist_perc = [amir_obe,sir,jalen_santoy,derek_pope,the88glam,
+    cunninlynguists,joyner_lucas,flatbush_zombies,kirk_knight,nyck_caution];
+
+
+    var keysSorted = Object.keys(artist_picks).sort(function(a,b){return artist_picks[b]-artist_picks[a]});
+    
+    artist_perc.sort(function(a,b){return b-a});
+
+    alert(keysSorted[0] + ": " + artist_perc[0] + ", " + keysSorted[1] + " " + artist_perc[1]);
+
+    
+}
+
+
 
 function incrAArtists() {
     switch (question_number) {
         case 1:
-            derek_pope++ , the88glam++ , nyck_caution++; break;
+            derek_pope++, the88glam++ , nyck_caution++; break;
         case 2:
-            ; break;
+            sir++, derek_pope++, cunninlynguists++; break;
         case 3:
-            ; break;
-        // case 4:
-        //     ; break;
+            joyner_lucas++, nyck_caution++, flatbush_zombies++; break;
+        case 4:
+            nyck_caution++, royce_da_59++, jalen_santoy++; break;
         case 5:
-            ; break;
+            flatbush_zombies++, the88glam++, jalen_santoy++; break;
         case 6:
-            ; break;
+            cunninlynguists++, joyner_lucas++, royce_da_59++; break;
         case 7:
-            ; break;
+            amir_obe++, derek_pope++, the88glam++; break;
         case 8:
-            ; break;
+            jalen_santoy++, sir++, royce_da_59++; break;
         case 9:
-            ; break;
+            joyner_lucas++, flatbush_zombies++, kirk_knight++, nyck_caution++; break;
         case 10:
-            ; break;
+            sir++, kirk_knight++; break;
         default:
             return;
     }
@@ -126,35 +159,25 @@ function incrAArtists() {
 function incrBArtists() {
     switch (question_number) {
         case 1:
-            flatbush_zombies++ , kirk_knight++ , nyck_caution++;
-            break;
+            flatbush_zombies++, kirk_knight++, nyck_caution++; break;
         case 2:
-            ;
-            break;
+            the88glam++, royce_da_59++, nyck_caution++; break;
         case 3:
-            ;
-            break;
+            kirk_knight++, cunninlynguists++, royce_da_59++; break;
         case 4:
-            ;
-            break;
+            amir_obe++, derek_pope++, joyner_lucas++; break;
         case 5:
-            ;
-            break;
+            amir_obe++, sir++; break;
         case 6:
-            ;
-            break;
+            the88glam++, sir++, amir_obe++; break;
         case 7:
-            ;
-            break;
+            flatbush_zombies++, kirk_knight++; break;
         case 8:
-            ;
-            break;
+            amir_obe++, derek_pope++, the88glam++; break;
         case 9:
-            ;
-            break;
+            derek_pope++, amir_obe++, sir++; break;
         case 10:
-            ;
-            break;
+            cunninlynguists++, royce_da_59++, nyck_caution++; break;
         default:
             return;
     }
@@ -163,35 +186,25 @@ function incrBArtists() {
 function incrCArtists() {
     switch (question_number) {
         case 1:
-            jalen_santoy++ , sir++;
-            break;
+            jalen_santoy++, sir++; break;
         case 2:
-            ;
-            break;
+            jalen_santoy++, joyner_lucas++, kirk_knight++; break;
         case 3:
-            ;
-            break;
+            sir++, the88glam++; break;
         case 4:
-            ;
-            break;
+            cunninlynguists++, flatbush_zombies++; break;
         case 5:
-            ;
-            break;
+            amir_obe++, sir++; break;
         case 6:
-            ;
-            break;
+            jalen_santoy++, flatbush_zombies++, kirk_knight++; break;
         case 7:
-            ;
-            break;
+            sir++, nyck_caution++, jalen_santoy++, joyner_lucas++; break;
         case 8:
-            ;
-            break;
+            flatbush_zombies++, joyner_lucas++; break;
         case 9:
-            ;
-            break;
+            jalen_santoy++, cunninlynguists++; break;
         case 10:
-            ;
-            break;
+            flatbush_zombies++, joyner_lucas++, jalen_santoy++; break;
         default:
             return;
     }
@@ -200,34 +213,25 @@ function incrCArtists() {
 function incrDArtists() {
     switch (question_number) {
         case 1:
-            cunninlynguists++ , joyner_lucas++ , royce_da_59++; break;
+            cunninlynguists++, joyner_lucas++, royce_da_59++; break;
         case 2:
-            ;
-            break;
+            amir_obe++, flatbush_zombies++; break;
         case 3:
-            ;
-            break;
+            sir++, the88glam++; break;
         case 4:
-            ;
-            break;
+            amir_obe++, derek_pope++, jalen_santoy++; break;
         case 5:
-            ;
-            break;
+            derek_pope++, joyner_lucas++, nyck_caution++; break;
         case 6:
-            ;
-            break;
+            derek_pope++, nyck_caution++; break;
         case 7:
-            ;
-            break;
+            cunninlynguists++, royce_da_59++; break;
         case 8:
-            ;
-            break;
+            cunninlynguists++, kirk_knight++, nyck_caution++; break;
         case 9:
-            ;
-            break;
+            the88glam++, royce_da_59++; break;
         case 10:
-            ;
-            break;
+            amir_obe++, derek_pope++, the88glam++; break;
         default:
             return;
     }
